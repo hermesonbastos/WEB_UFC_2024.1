dff9334924315e3f218a35932cc8147b0035a064
hermesonbastos <hermesonbastos@gmail.com>
2024-03-19 21:12:55 -0300

    questão 3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react'
-import MeusDados from './Atividade00/02MeusDados'
-import './App.css'
-import Temperatura from './Atividade00/03Temperatura'
-import Pai from './atividade01/questao01/01Pai'
-import * as PC from './atividade01/02MeuPC';
+import React from "react";
+import MeusDados from "./Atividade00/02MeusDados";
+import "./App.css";
+import Temperatura from "./Atividade00/03Temperatura";
+import Pai from "./atividade01/questao01/01Pai";
+import * as PC from "./atividade01/02MeuPC";
+import { Arena, Enemy, Hero, World } from "./atividade01/03Batalha";
+import { hero_img, vilain_img } from "./atividade01/Imagens";
 
 const App = () => {
   return (
     <div>
-      {/* <MeusDados nome="Hermeson Bastos" curso="Sistemas de Informação" campus="Universidade Federal do Ceará" /> */}
-      {/* <Temperatura /> */}
-
       {/* Atividade 01 */}
       <h4>Questão 01</h4>
       <Pai />
-
       <br />
 
       <h4>Questão 02</h4>
       <PC.PlacaMae nome="GIGABYTE B450" preco="R$ 648,87" />
       <PC.Memoria nome="VALKYRIE V5 8GB" preco="R$ 158,87" />
       <PC.PlacaDeVideo nome="RTX 4060 8GB" preco="R$ 2098,00" />
-    </div>
-  )
-}
+      <br />
 
-export default App
+      <h4>Questão 03</h4>
+      <div>
+        <World>
+          <Arena arena="U.A. High School">
+            <Hero name="Mirio Togata" img={hero_img} />
+            <Enemy name="Overhaul" img={vilain_img} />
+          </Arena>
+          <Arena arena="Class 1-A">
+            <Hero name="Mirio Togata" img={hero_img} />
+            <Enemy name="Overhaul" img={vilain_img} />
+          </Arena>
+          <Arena arena="Musutafu, Japan">
+            <Hero name="Mirio Togata" img={hero_img} />
+            <Enemy name="Overhaul" img={vilain_img} />
+          </Arena>
+        </World>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/atividade01/03Batalha.jsx
@@ -0,0 +1,43 @@
+import { hero_img, vilain_img } from "./Imagens";
+import "../App.css";
+import React from "react";
+
+export function Hero({ name, img, arena }) {
+  return (
+    <div>
+      <h2 className="hero-name">{name}</h2>
+      <img src={img} alt={name} />
+      <p>{arena}</p>
+    </div>
+  );
+}
+
+export function Enemy({ name, img, arena }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={img} alt={name} />
+      {arena}
+    </div>
+  );
+}
+
+export function Arena({ children, arena }) {
+  return (
+    <div className="arena">
+      {React.cloneElement(children[0], {
+        arena,
+      }, <p>{arena}</p>)}
+      <span>
+        <strong>VS</strong>
+      </span>
+      {React.cloneElement(children[1], {
+        arena,
+      }, <p>{arena}</p>)}
+    </div>
+  );
+}
+
+export function World(props) {
+  return <div>{props.children}</div>;
+}
